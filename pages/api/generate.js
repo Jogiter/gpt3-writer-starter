dec1692e8923f8fc13f90bedb3eec41380a87c94
Jogiter <jogiter.g@gmail.com>
2023-06-01 13:13:17 +0800

    fix: catch error and throw it

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -11,16 +11,21 @@ const generateAction = async (req, res) => {
   // Run first prompt
   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
 
-  const baseCompletion = await openai.createCompletion({
-    model: 'text-davinci-003',
-    prompt: `${basePromptPrefix}${req.body.userInput}`,
-    temperature: 0.7,
-    max_tokens: 1000,
-  });
+  try {
+    const baseCompletion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `${basePromptPrefix}${req.body.userInput}`,
+      temperature: 0.7,
+      max_tokens: 1000,
+    });
   
-  const basePromptOutput = baseCompletion.data.choices.pop();
+    const basePromptOutput = baseCompletion.data.choices.pop();
 
-  res.status(200).json({ output: basePromptOutput });
+    res.status(200).json({ output: basePromptOutput });
+  } catch (error) {
+    res.status(500).json({ output: error });
+  }
+
 };
 
 export default generateAction;
